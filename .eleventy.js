2bdbceba721ef3de98955f354dcdebfc6796d152
Agata Zurek <agata.w.zurek@gmail.com>
2020-08-03 14:48:11 -0700

    tidying up

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,10 @@ module.exports = function (eleventyConfig) {
   // copy static assets to output folder
   eleventyConfig.addPassthroughCopy("./src/static/images");
   eleventyConfig.addPassthroughCopy("./src/static/css");
-  eleventyConfig.addPassthroughCopy("./src/cms");
-
+  
   // cms config and html
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/cms");
 
   return {
     dir: {
