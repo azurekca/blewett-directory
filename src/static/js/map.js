a329f23893dbe91fa144815bf1840f28944f0c44
Agata Zurek <agata.w.zurek@gmail.com>
2020-09-01 08:41:21 -0700

    switched back to basic map icon

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,11 +1,22 @@
 const map = L.map('mapBox').setView([49.479076, -117.393192], 14);
 
+const myIcon = L.icon({
+    iconUrl: '/static/images/place.svg',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: '',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 listings.forEach(listing => {
-  console.log(listing.geometry.coordinates[1]);
-  L.marker([listing.geometry.coordinates[1], listing.geometry.coordinates[0]]).addTo(map)
-    .bindPopup(listing.title);
+  if (listing.showMap) {
+    L.marker([listing.geometry.coordinates[1], listing.geometry.coordinates[0]]).addTo(map)
+      .bindPopup(listing.title);
+  }
 })
